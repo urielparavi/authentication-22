906dff6ad3c053260f28160af7509069d4fd5dc7
Your Name <urielparavi75@gmail.com>
2024-07-22 15:08:35 +0300

    sdf

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -38,6 +38,13 @@ export async function action({ request }) {
     throw json({ message: 'Could not authenticate user.' }, { status: 500 });
   }
 
-  // soon: manage that token
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem('token', token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1); // Create 1 hour in the future
+  localStorage.setItem('expiration', expiration.toISOString());
+
   return redirect('/');
 }
---
+++ frontend/src/util/auth.js
@@ -0,0 +1,39 @@
+import { redirect } from 'react-router-dom';
+
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem('expiration');
+  const expirationDate = new Date(storedExpirationDate); // We transform to a date object from string
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+}
+
+export function getAuthToken() {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return 'EXPIRED';
+  }
+
+  return token;
+}
+
+export function tokenLoader() {
+  return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect('/auth');
+  }
+
+  return null; // In case where nothing would be returned to avoid errors.
+}
